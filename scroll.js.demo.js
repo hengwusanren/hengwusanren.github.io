d6602a7e18048aec5d21dba0b7d0e96c43c17023
hengwusanren <youchuangqiba@163.com>
2016-08-22 01:35:33 +0800

    upd8: requirejs.

--- scroll.js.demo.js
+++ scroll.js.demo.js
@@ -6,7 +6,7 @@ requirejs.config({
 });
 
 requirejs(['mock', 'scroll'], function(Mock, Scroll) {
-    var test_listLength = 100;
+    var test_listLength = 10;
     var test_data = Mock.mock({
         'list|1-100': [{
             'id|+1': 1
@@ -16,15 +16,18 @@ requirejs(['mock', 'scroll'], function(Mock, Scroll) {
     console.log(test_data);
 
     var test_addScrollContent = function(div) {
-        for (var i = 0; i < 100; i++) {
+        for (var i = 0; i < test_listLength; i++) {
             var child = document.createElement('div');
             child.className = 'test-div';
-            child.innerHTML = i;
+            child.innerHTML = div.childNodes.length + 1;
             div.appendChild(child);
         }
     };
 
-    test_addScrollContent(document.getElementById('div1'));
+    // setInterval(function() {
+        test_addScrollContent(document.getElementById('div1'));
+    // }, 2000);
+
     new Scroll.control(document.getElementById('div1'), function(elem, scrollDir, totalDistX, totalDistY, elapsedTime, distXIntervals, distYIntervals, timeIntervals, scrollPosition, transBoundary) {
         // console.log({
         //     dist: totalDistX + ',' + totalDistY,
@@ -38,6 +41,11 @@ requirejs(['mock', 'scroll'], function(Mock, Scroll) {
 
     test_addScrollContent(document.getElementById('div2'));
     new Scroll.control(document.getElementById('div2'), function(elem, scrollDir, totalDistX, totalDistY, elapsedTime, distXIntervals, distYIntervals, timeIntervals, scrollPosition, transBoundary) {
-        //TODO
+        console.log(scrollPosition);
+        if(scrollPosition == transBoundary) {
+            console.log('Get to the bottom!');
+        } else if(scrollPosition == 0) {
+            console.log('Get to the top!');
+        }
     });
 });
